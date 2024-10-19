0be60019ba6e4f4f37f2afdf5376872d67de3a78
kadiem <kadiem1999@gmail.com>
2024-10-19 14:09:59 +0000

    commit 23

--- PisciSmart/src/main/java/com/PisciSmart/SensorDataService.java
+++ PisciSmart/src/main/java/com/PisciSmart/SensorDataService.java
@@ -20,26 +20,24 @@ public class SensorDataService {
     public List<SensorData> getDataByDeviceNumSerie(String numSerie) {
         return sensorDataRepository.findByNumSerie(numSerie);
     }
-    public double calculateOverallPercentage( double ph, double tds) {
+    public double calculateOverallPercentage(double ph, double tds) {
         // Définir les plages pour chaque paramètre
-        // double minTemperature = 10.0;
-        //double maxTemperature = 60.0;
-        double minPh = 6.0;
-        double maxPh = 8.5;
-        int minTds = 400;
-        int maxTds = 1500;
+        double minPh = 0;
+        double maxPh = 14;
+        int minTds = 0;
+        int maxTds = 5000;
 
-        // Normaliser chaque valeur entre 0 et 1
-       // double normalizedTemperature = (temperature - minTemperature) / (maxTemperature - minTemperature);
-        double normalizedPh = (ph - minPh) / (maxPh - minPh);
-        double normalizedTds = (double)(tds - minTds) / (maxTds - minTds);
+        // Normaliser chaque valeur entre 0 et 1 et limiter les résultats entre 0 et 1
+        double normalizedPh = Math.max(0, Math.min(1, (ph - minPh) / (maxPh - minPh)));
+        double normalizedTds = Math.max(0, Math.min(1, (double)(tds - minTds) / (maxTds - minTds)));
 
         // Calculer le pourcentage global (moyenne des valeurs normalisées)
-        double overallPercentage = ( normalizedPh + normalizedTds) / 2 * 100;
+        double overallPercentage = ((normalizedPh + normalizedTds) / 2) * 100;
 
         // Limiter le pourcentage entre 0 et 100
         overallPercentage = Math.max(0, Math.min(100, overallPercentage));
 
         return overallPercentage;
     }
+
 }
